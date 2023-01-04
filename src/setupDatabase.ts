27c1f9941351a300ab39ac27ea6f40dd211c34f1
Patricia Garcia <martinez171214@gmail.com>
2023-01-04 17:26:57 +0100

    setup project

---
+++ src/setupDatabase.ts
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose'
+import Logger from 'bunyan'
+import { config } from './config'
+
+mongoose.set('strictQuery', true)
+
+const log: Logger = config.createLogger('database');
+
+export default () => {
+    const connect = () => {
+        mongoose.connect(`${config.MONGODB_URL}`)
+          .then(() => log.info('👽 Connected to MongoDB!'))
+          .catch((err) => {
+            log.error(err)
+            return process.exit(1)
+        })
+    }
+    connect()
+    mongoose.connection.on('disconnected', connect)
+}
