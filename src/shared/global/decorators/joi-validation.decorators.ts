c317794f15ac6c9065ece1bc15c39d37d1b3a1d7
Patricia Garcia <martinez171214@gmail.com>
2023-01-06 09:49:17 +0100

    auth faeture

--- src/shared/global/decorators/joi-validation.decorators.ts
+++ src/shared/global/decorators/joi-validation.decorators.ts
@@ -4,12 +4,11 @@ import { ObjectSchema } from 'joi'
 
 type IJoiDecorator = (target: any, key: string, descriptor: PropertyDescriptor) => void;
 
-export function joinValidation(schema: ObjectSchema): IJoiDecorator {
+export function joiValidation(schema: ObjectSchema): IJoiDecorator {
   return (_target: any, _key: string, descriptor: PropertyDescriptor) => {
     const originalMethod = descriptor.value;
     descriptor.value = async function (...args: any[]) {
       const req: Request = args[0]
-      // validateAsync or validate
       const {error} = await Promise.resolve(schema.validate(req.body))
       if(error?.details) {
         throw new JoiRequestValidationError(error.details[0].message)
