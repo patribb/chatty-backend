c8f67ccdf2f82711f0cd64b5ff68d948c0639b21
Patricia Garcia <martinez171214@gmail.com>
2023-01-07 11:26:41 +0100

    feat: implemented password reset feature and test

--- src/shared/services/queues/auth.queue.ts
+++ src/shared/services/queues/auth.queue.ts
@@ -1,5 +1,5 @@
 import { IAuthJob } from '@auth/interfaces/auth.interface';
-import { authWorker } from '@workers/auth.worker';
+import { authWorker } from '@worker/auth.worker';
 import { BaseQueue } from './base.queues'
 
 class AuthQueue extends BaseQueue {
