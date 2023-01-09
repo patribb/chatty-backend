14229b0d3b9a7ede91e2e691e408f2955948df3f
Patricia Garcia <martinez171214@gmail.com>
2023-01-09 17:35:10 +0100

    feat: added comments-features

---
+++ src/shared/services/queues/comment.queue.ts
@@ -0,0 +1,15 @@
+import { ICommentJob } from '@comment/interfaces/comment.interface';
+import { commentWorker } from '@worker/comment.worker';
+import { BaseQueue } from './base.queues';
+
+class CommentQueue extends BaseQueue {
+  constructor() {
+    super('comments')
+    this.processJob('addCommentToDB', 5, commentWorker.addCommentToDB)
+  }
+  public addCommentJob(name: string, data: ICommentJob): void {
+    this.addJob(name, data)
+  }
+}
+
+export const commentQueue: CommentQueue = new CommentQueue();
