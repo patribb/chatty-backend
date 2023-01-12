6a077041e7217d29f6fa47dc99b893c86a400e4d
Patricia Garcia <martinez171214@gmail.com>
2023-01-12 15:09:33 +0100

    feat: get users feature

--- src/shared/services/db/user.service.ts
+++ src/shared/services/db/user.service.ts
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';
 import { IUserDocument } from '@user/interfaces/user.interface';
 import { UserModel } from '@user/models/user.schema';
+import { followerService } from './follower.service';
+import { indexOf } from 'lodash';
 
 class UserService {
   public async addUserData(data: IUserDocument): Promise<void> {
@@ -25,6 +27,57 @@ class UserService {
       { $project: this.aggregateProject() }
     ]);
     return users[0];
+  }
+
+  public async getAllUsers(userId: string, skip: number, limit: number): Promise<IUserDocument[]> {
+    const users: IUserDocument[] = await UserModel.aggregate([
+      { $match: { _id: {$ne: new mongoose.Types.ObjectId(userId)} } },
+      { $skip: skip },
+      { $limit: limit },
+      { $sort: { createdAt: -1 }},
+      { $lookup: { from: 'Auth', localField: 'authId', foreignField: '_id', as: 'authId' } },
+      { $unwind: '$authId' },
+      { $project: this.aggregateProject() }
+    ]);
+    return users;
+  }
+
+  public async getTotalUsersInDB(): Promise<number> {
+    const totalCount: number = await UserModel.find({}).countDocuments();
+    return totalCount;
+  }
+
+  public async getRandomUsers(userId: string): Promise<IUserDocument[]> {
+    const randomUsers: IUserDocument[] = [];
+    const users: IUserDocument[] = await UserModel.aggregate([
+      { $match: { _id: { $ne: new mongoose.Types.ObjectId(userId) } } },
+      { $lookup: { from: 'Auth', localField: 'authId', foreignField: '_id', as: 'authId' } },
+      { $unwind: '$authId' },
+      { $sample: { size: 10 } },
+      {
+        $addFields: {
+          username: '$authId.username',
+          email: '$authId.email',
+          avatarColor: '$authId.avatarColor',
+          uId: '$authId.uId',
+          createdAt: '$authId.createdAt'
+        }
+      },
+      {
+        $project: {
+          authId: 0,
+          __v: 0
+        }
+      }
+    ]);
+    const followers: string[] = await followerService.getFolloweesIds(`${userId}`);
+    for (const user of users) {
+      const followerIndex = indexOf(followers, user._id.toString());
+      if (followerIndex < 0) {
+        randomUsers.push(user);
+      }
+    }
+    return randomUsers;
   }
 
   private aggregateProject() {
